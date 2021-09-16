3228a82cd30ce06fe3e62ff2a5ed0998df97352f
v-king777 <v.kolpashchikov@gmail.com>
2021-09-16 19:55:29 +0300

    Update_16.09.21_19.55

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,9 +1,24 @@
-﻿let age = prompt("Пожалуйста, введите ваш возраст");
+﻿// создадим объект Map для хранения сессии
+let session = new Map();
 
-if (age >= 18) {
-    alert("Добро пожаловать на LifeSpot! " + new Date().toLocaleString());
+// Сохраним UserAgent
+session.set("userAgent", window.navigator.userAgent);
+
+// Запросим возраст пользователя и тоже сохраним
+session.set("age", prompt("Пожалуйста, введите ваш возраст"));
+
+// Проверка на возраст и сохранение сессии
+if (session.get("age") >= 18) {
+    let startDate = new Date().toLocaleString();
+    alert("Добро пожаловать на LifeSpot!\nТекущее время: " + startDate);
+    session.set("startDate", startDate);
 }
 else {
     alert("Наши трансляции не предназначены для лиц моложе 18 лет.\nВы будете перенаправлены в гугл:)");
-    window.location.href = "http://www.google.com"
+    window.location.href = "http://www.google.com";
+}
+
+// Вывод в консоль
+for (let result of session) {
+    console.log(result);
 }
