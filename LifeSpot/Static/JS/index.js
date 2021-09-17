fc260cdcba78f09388276a003e2cef84de4994e4
v-king777 <v.kolpashchikov@gmail.com>
2021-09-17 14:47:08 +0300

    Update_17.09.21_14.47

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,24 +1,44 @@
-﻿// создадим объект Map для хранения сессии
-let session = new Map();
+﻿let sessionHandler = function handleSession() {
+    // создадим объект Map для хранения сессии
+    let session = new Map();
 
-// Сохраним UserAgent
-session.set("userAgent", window.navigator.userAgent);
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent);
 
-// Запросим возраст пользователя и тоже сохраним
-session.set("age", prompt("Пожалуйста, введите ваш возраст"));
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"));
 
-// Проверка на возраст и сохранение сессии
-if (session.get("age") >= 18) {
-    let startDate = new Date().toLocaleString();
-    alert("Добро пожаловать на LifeSpot!\nТекущее время: " + startDate);
-    session.set("startDate", startDate);
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+        alert("Добро пожаловать на LifeSpot!\nТекущее время: " + startDate);
+        session.set("startDate", startDate);
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет.\nВы будете перенаправлены в гугл:)");
+        window.location.href = "http://www.google.com";
+    }
+
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result);
+    }
 }
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет.\nВы будете перенаправлены в гугл:)");
-    window.location.href = "http://www.google.com";
-}
 
-// Вывод в консоль
-for (let result of session) {
-    console.log(result);
+let contentFilter = function filterContent(userInput) {
+
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; elements.length; i++) {
+        let videoDescription = elements[i].getElementsByClassName('video-description')[0].innerText;
+
+        if (!videoDescription.toLowerCase().includes(userInput.toLowerCase())) {
+            elements[i].style.display = 'none';
+        }
+        else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
 }
