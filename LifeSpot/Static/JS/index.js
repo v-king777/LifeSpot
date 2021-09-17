aba3486893f70022a317a3feeb8509374697c6df
v-king777 <v.kolpashchikov@gmail.com>
2021-09-17 17:18:25 +0300

    Update_17.09.21_17.18

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,40 +1,37 @@
-﻿let sessionHandler = function handleSession() {
-    // создадим объект Map для хранения сессии
-    let session = new Map();
+﻿let session = new Map();
 
-    // Сохраним UserAgent
+function handleSession() {
+    session.set("startDate", new Date().toLocaleString());
     session.set("userAgent", window.navigator.userAgent);
+}
 
-    // Запросим возраст пользователя и тоже сохраним
+function checkAge() {
     session.set("age", prompt("Пожалуйста, введите ваш возраст"));
 
-    // Проверка на возраст и сохранение сессии
     if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-        alert("Добро пожаловать на LifeSpot!\nТекущее время: " + startDate);
-        session.set("startDate", startDate);
+        alert("Добро пожаловать на LifeSpot!\nТекущее время: " + new Date().toLocaleString());
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет.\nВы будете перенаправлены в гугл:)");
         window.location.href = "http://www.google.com";
     }
+}
 
-    // Вывод в консоль
+let sessionLog = function logSession() {
+
     for (let result of session) {
         console.log(result);
     }
 }
 
-let contentFilter = function filterContent(userInput) {
+function filterContent() {
 
-    // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
 
-    // Пробегаемся по контейнерам
     for (let i = 0; elements.length; i++) {
         let videoDescription = elements[i].getElementsByClassName('video-description')[0].innerText;
 
-        if (!videoDescription.toLowerCase().includes(userInput.toLowerCase())) {
+        if (!videoDescription.toLowerCase().includes(inputParseFunction().toLowerCase())) {
             elements[i].style.display = 'none';
         }
         else {
